b1d8135abadf2eab17833161b0b6a9277a02ede4
josephgithub <josephozgen@gmail.com>
2020-12-13 11:03:14 -0800

    initial commit

---
+++ module1_basics.R
@@ -0,0 +1,55 @@
+air <- read.csv('airlines.csv', stringsAsFactors = FALSE)
+delay <- air$DEPARTURE_DELAY
+delay[1:10]
+crazymakers <- delay[delay>300]
+crazymakers[1:10]
+crazymakers <- crazymakers[!is.na(crazymakers)]
+crazymakers[1:10]
+
+vec1 <- sample(1:5, 10, replace = TRUE)
+vec2 <- sample(1:5, 10, replace = TRUE)
+vec1
+vec2
+vec1 + vec2
+vec1^vec2
+vec1 >= vec2
+vec1 <= 5
+vec1 <= 0 | vec1 >= 3
+vec1 <= 0 & vec1 >= vec2
+vec1 == vec2
+vec1 != vec2
+
+median(crazymakers)
+median(delay, na.rm = TRUE)
+
+hist(rnorm(1000))
+
+help(lm)
+?lm
+
+dim(air)
+nrow(air)
+names(air)
+class(air)
+is.matrix(air)
+
+myList <- list(stuff = 3, mat = matrix(1:4, nrow = 2), moreStuff = c("turkey","us"), list(5, "bear"))
+myList
+myList[[1]]
+myList$moreStuff[2]
+
+is.list(myList)
+class(myList)
+
+hist(air$DEPARTURE_DELAY)
+set.seed(0, sample.kind = "Rounding")
+subset_air <- air[sample(1:nrow(air), 10000, replace=FALSE),]
+subset_air$DEPARTURE_DELAY[subset_air$DEPARTURE_DELAY > 60*3] <- 60*3
+plot(subset_air$DEPARTURE_DELAY ~ subset_air$DISTANCE)
+boxplot(subset_air$DEPARTURE_DELAY ~ subset_air$DAY_OF_WEEK)
+boxplot(subset_air$DEPARTURE_DELAY ~ subset_air$AIRLINE)
+par(pch = 2)
+plot(subset_air$DEPARTURE_DELAY ~ subset_air$DISTANCE,
+     xlab = 'distance(miles)',
+     ylab = 'delay (minutes)',
+     log = 'x')
